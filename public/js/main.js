46151afa2fa6ff4e7f0f44449e424d4bff0159f5
James Lo <jameslo599@gmail.com>
2023-07-26 02:34:43 -0500

    integrated mongodb

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,13 @@ async function getName() {
   const tarotName = document.querySelector("input").value;
   try {
     const response = await fetch(`https://tarot.cyclic.app/api/${tarotName}`);
+    // const response = await fetch(`http://localhost:8000/api/${tarotName}`);
     const data = await response.json();
 
     console.log(data);
-    document.querySelector("h2").innerText = data.name;
-    document.querySelector(".image").src = data.image;
-    document.querySelector(".description").innerText = data.description;
+    document.querySelector("h2").innerText = data[0].name;
+    document.querySelector(".image").src = data[0].image;
+    document.querySelector(".description").innerText = data[0].description;
   } catch (error) {
     console.log(error);
   }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = 8000;
 const MongoClient = require("mongodb").MongoClient;
@@ -11,22 +12,32 @@ MongoClient.connect(uri, { useUnifiedTopology: true })
     console.log("Connected to Database");
     const db = client.db("tarot");
     const tarotCards = db.collection("tarotCards");
+
+    // MiddleWars
+    app.use(express.static("public"));
+    app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    // Routes
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/index.html");
+    });
+
+    app.get("/api/:tarotCard", (req, res) => {
+      const card = req.params.tarotCard.toLowerCase();
+      tarotCards
+        .find({ id: `${card}` })
+        .toArray()
+        .then((data) => res.json(data))
+        .catch((error) => res.json("Not found"));
+      //   tarotCards[card] ? res.json(tarotCards[card]) : res.json("Not found");
+    });
+
+    // Listen
+    app.listen(process.env.PORT || PORT, () => {
+      console.log(`The server is running on ${PORT}.`);
+    });
   })
   .catch((error) => console.error(error));
-
-app.use(express.static("public"));
-app.use(cors());
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.get("/api/:tarotCard", (req, res) => {
-  const card = req.params.tarotCard.toLowerCase();
-  res.json("not found");
-  //   tarotCards[card] ? res.json(tarotCards[card]) : res.json("Not found");
-});
-
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`The server is running on ${PORT}.`);
-});
