4012c9b0952316ba97eaf8041f34287ede84305a
James Lo <jameslo599@gmail.com>
2023-07-26 02:58:24 -0500

    cyclic error

--- server.js
+++ server.js
@@ -6,38 +6,39 @@ const PORT = 8000;
 const MongoClient = require("mongodb").MongoClient;
 const uri =
   "mongodb+srv://tarotapi59:koolster999@tarot.ay1wplb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 
-MongoClient.connect(uri, { useUnifiedTopology: true })
-  .then((client) => {
-    console.log("Connected to Database");
-    const db = client.db("tarot");
-    const tarotCards = db.collection("tarotCards");
+// MiddleWars
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
-    // MiddleWars
-    app.use(express.static("public"));
-    app.set("view engine", "ejs");
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
-    app.use(cors());
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-    // Routes
-    app.get("/", (req, res) => {
-      res.sendFile(__dirname + "/index.html");
-    });
+app.get("/api/:tarotCard", async (req, res) => {
+  const db = await client.db("tarot");
+  const tarotCards = await db.collection("tarotCards");
+  const card = req.params.tarotCard.toLowerCase();
+  tarotCards
+    .find({ id: `${card}` })
+    .toArray()
+    .then((data) => res.json(data))
+    .catch((error) => res.json("Not found"));
+});
 
-    app.get("/api/:tarotCard", (req, res) => {
-      const card = req.params.tarotCard.toLowerCase();
-      tarotCards
-        .find({ id: `${card}` })
-        .toArray()
-        .then((data) => res.json(data))
-        .catch((error) => res.json("Not found"));
-    });
-  })
-  .then(() => {
-    // Listen
-    app.listen(process.env.PORT || PORT, () => {
-      console.log(`The server is running on ${PORT}.`);
-    });
-  })
-  .catch((error) => console.error(error));
+client.connect((err) => {
+  if (err) {
+    console.error(err);
+    return false;
+  }
+  // connection to mongo is successful, listen for requests
+  // Listen
+  app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is running on ${PORT}.`);
+  });
+});
