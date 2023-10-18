ce1e1c1a9fa382c94dc29d99f9e07aba55170a2f
James Lo <jameslo599@gmail.com>
2023-10-18 02:52:07 -0500

    styled glossary;

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "drop-down": "url('/public/images/down-arrow.svg')",
+      },
+      padding: {
+        "10%": "10%",
+      },
+    },
   },
   plugins: [],
 };
