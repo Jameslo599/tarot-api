390ff082e2f76520f21e003b1b719530ddfdf8a8
James Lo <jameslo599@gmail.com>
2023-11-02 02:30:02 -0500

    finished mobile about page

--- public/js/about.js
+++ public/js/about.js
@@ -1,1 +1,31 @@
-//Open mobile navbar
+//Highlight chapter when clicked
+const table = document.querySelector("#table-contents").children;
+for (let i = 0; i < table.length; i++) {
+  table[i].addEventListener("click", () => {
+    reset();
+    console.log(table[i].textContent);
+    switchChapter(table[i].textContent);
+    table[i].classList.add("bg-gray-600");
+    table[i].classList.add("font-bold");
+  });
+}
+
+//Reset highlighted chapter
+function reset() {
+  const list = document.querySelectorAll("ul#table-contents > a");
+  for (const ele of list) {
+    ele.classList.remove("bg-gray-600");
+    ele.classList.remove("font-bold");
+  }
+}
+
+//Switch chapters
+function switchChapter(str) {
+  const chapter = document.querySelectorAll("div.chapter");
+  for (const ele of chapter) {
+    ele.classList.add("hidden");
+    ele.children[0].textContent.trim() === str
+      ? ele.classList.remove("hidden")
+      : null;
+  }
+}
