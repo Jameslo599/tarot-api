b46965be0d23865d2ba6de9dc1f6939d7950311a
James Lo <jameslo599@gmail.com>
2023-11-16 00:31:34 -0600

    card-api

--- public/js/user.js
+++ public/js/user.js
@@ -3,13 +3,13 @@ async function signUp() {
   const user = "jameslo599";
   const password = "runescape";
   try {
-    const response = await fetch(`http://localhost:8000/signup/${user}`);
+    const response = await fetch(`https://tarot.cyclic.app/signup/${user}`);
     const data = await response.json();
     console.log(data);
     //if user exists then return, else send req to add user
     data.length >= 1
       ? console.log("username is taken")
-      : fetch(`http://localhost:8000/signup/${user}/${password}`);
+      : fetch(`https://tarot.cyclic.app/signup/${user}/${password}`);
   } catch (error) {
     console.log(error);
   }
